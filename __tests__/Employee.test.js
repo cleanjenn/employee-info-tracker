575f95febd3ec4323a173749cea881163599f03f
jennifer gomez <jennifer23gomez7@gmail.com>
2021-03-06 13:09:09 -0800

    start up code created and Employee test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,50 @@
 const Employee = require('../lib/Employee');
 
-test('creates an employee', () => {
-    const employee = new Employee('sally');
+// #1
+test('creates a new employee', () => {
+    const employee = new Employee();
     
-    expect(employee.name).toBe('sally');
+    expect(typeof (employee)).toBe('object');
+});
+// #2
+test('nameing the employee', () => {
+    const name = "Sally";
+    const emp = new Employee(name);
+    expect(emp.name).toBe(name);
+});
+// #3
+test('creating the employee ID', () => {
+    const testValue = 20;
+    const emp = new Employee("S", testValue);
+    expect(emp.id).toBe(testValue);
+});
+// #4
+test('creating the employee email', () => {
+    const testValue = "testthisemail@gmail.com"
+    const emp = new Employee("S", 1, testValue);
+    expect(emp.email).toBe(testValue);
+});
+// #5
+test('Get the employees name using method', () => {
+    const testValue = "Sally";
+    const emp = new Employee(testValue);
+    expect(emp.getName()).toBe(testValue);
+});
+// #6
+test('Get the employees ID using method', () => {
+    const testValue = 100;
+    const emp = new Employee("S", testValue);
+    expect(emp.getId()).toBe(testValue);
+});
+// #7
+test('Get the employees email using method', () => {
+    const testValue = "testthisemail@gmail.com";
+    const emp = new Employee("S", 1, testValue);
+    expect(emp.getEmail()).toBe(testValue);
+});
+// #8
+test('Get the employees role using method', () => {
+    const testValue = "Employee";
+    const emp = new Employee("Sally", 1, "testthisemail@gmail.com");
+    expect(emp.getRole()).toBe(testValue);
 });
--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-const Employee = require('./lib/Employee');
+// add requirements for page to start
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const html = require("./src/Temp");
+const validator = require("email-validator");
 
-new Employee().initializeEmployee();
+// functions to write to a file
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+
+let empArr = [];   //employeeArray
+let empStr = ``;
+
+console.clear();
+console.log("_________________________________");
+console.log("Your Employee list begins here!")
+
+// begin the function to promp the questions
+async function master() {
+    try {
+        await prompt()
+
+        for (let i = 0; i < empArr.length; i++) {
+            emp = empStr + html.generateCard(empArr[i]);
+        }
+        let finalPage = html.generateHTML(empStr)
+
+        console.clear();
+        console.log("_________________________________");
+        console.log("Creating index.html file....");
+        console.log("_________________________________");
+
+        writeFileAsync("./dist/index.html", finalPage);
+
+        console.clear();
+        console.log("_________________________________");
+        console.log("index.html file created successfully");
+        console.log("_________________________________");
+    } catch (err){
+        return console.log(err);
+    }
+};
+
+// begin app 
+master();
