b4568024e0a10fd08a819f6cf057100a2aa278fa
jennifer gomez <jennifer23gomez7@gmail.com>
2021-03-07 19:39:58 -0800

    functionallity working and html page generated

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Intern = require("./lib/Intern");
 const html = require("./src/Temp");
 const validator = require("email-validator");
 
+
 // functions to write to a file
 const writeFileAsync = util.promisify(fs.writeFile);
 const appendFileAsync = util.promisify(fs.appendFile);
@@ -25,7 +26,7 @@ async function master() {
         await prompt()
 
         for (let i = 0; i < empArr.length; i++) {
-            emp = empStr + html.generateCard(empArr[i]);
+            empStr = empStr + html.generateCard(empArr[i]);
         }
         let finalPage = html.generateHTML(empStr)
 
@@ -44,6 +45,115 @@ async function master() {
         return console.log(err);
     }
 };
+
+async function prompt() {
+
+    let response = "";
+
+    do{
+        try{
+            let response1 = await inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter the employee's name?: ",
+                validate: function validateName(name) {
+                    return name !== '';
+                }
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter the employee's ID?: ",
+                validate: function validateName(name) {
+                    return name !== '';
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter the employee's email address? ",
+                validate: function validateEmail(name) {
+                    return validator.validate(name);
+                }
+            },
+            {
+                type: "list",
+                name: "role",
+                message: "What what is the employee's role?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "Manager"
+                ]
+            }
+        ]);
+
+        let response2 =""
+
+        if (response1.role === 'Engineer') {
+            response2 = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "a",
+                    message: "Please enter the employee's github username?",
+                    validate: function validateName(name) {
+                        return name !== ''; 
+                    }
+                }
+        ]);
+
+        const engineer = new Engineer(response1.name, response1.id, response1.email, response2.a);
+        empArr.push(engineer);
+
+        } else if (response1.role === 'Intern') {
+            response2 = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "b",
+                    message: "Please enter the school the employee is attending?",
+                    validate: function validatName(name) {
+                        return name !== '';
+                    }
+                }
+            ]);
+
+            const intern = new Intern(response1.name, response1.id, response1.email, response2.b);
+            empArr.push(intern);
+
+        } else if (response1.role === "Manager") {
+            response2 = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "c",
+                    message: "Please enter the employee's office number?:",
+                    validate: function validateName(name) {
+                        return name !== '';
+                    },
+                },
+            ]);
+
+            // Add to team Array
+            const manager = new Manager(response1.name, response1.id, response1.email, response2.c);
+            empArr.push(manager);
+        }
+    } catch (err) {
+        return console.log(err);
+    }
+
+    response = await inquirer.prompt([
+        {
+            type: "list",
+            name: "finish",
+            message: "Would you like to continue?",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        },]);
+
+    } while (response.finish === "Yes");
+}
 
 // begin app 
 master();
